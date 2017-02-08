13ed59ab4d5e6ae59363c94d31de60489f1c5e34
swashy stable <roland.macdavid@gmail.com>
2017-02-07 22:03:22 -0500

    was flooding syslogs because of tar having -v flag. Diff was also not actually working, and required me to export variables so they can be used in subshells

--- wordpress-backup.sh
+++ wordpress-backup.sh
@@ -4,16 +4,17 @@
 #Compare all contents of each file on really low priority with nice for ultimate injection paranoia
 
 theDate=$(date "+%F")
-backupFile="wordpress.bak.$theDate.tar.gz"
+export backupFile="/var/www/html/wordpress.bak.$theDate.tar.gz"
 #mysql dump by connecting with our options login file
 mysqldump --defaults-file=/root/wordpress-backup/.login.cnf wpdatabase > /var/www/html/mariadb-backup.d/db-bak.$theDate
-tar --xattrs -czpvf /var/www/html/$backupFile /var/www/html/wordpress/ /var/www/html/mariadb-backup.d/ /var/www/html/wp-secure.d/
+tar --xattrs -czpf $backupFile /var/www/html/wordpress /var/www/html/mariadb-backup.d /var/www/html/wp-secure.d
 #Debug. Give us notice that the timer actually ran
 
-tarList=$(tar -tvf /var/www/html/$backupfile)
+export tarList=$(tar -tvf $backupfile)
 #tail the last line, which will have the last backup file name in it surrounded by quotes. Cut them out.
-lastBackupFile=$(tail -n 1 backup-notice.log | cut -d\" -f2-2)
-tarListOld=$(tar -tvf /var/www/html/$lastBackupFile)
+backupPath="/var/www/html/"
+export lastBackupFile=$backupPath$(tail -n 1 /var/www/html/backup-notice.log | cut -d\" -f2-2)
+export tarListOld=$(tar -tvf $lastBackupFile)
 
 #Get the differences. the Strange echo syntax is how we diff variables in memory. -s will tell us if the tars have identical files listed
 difference=$(diff -s <(echo "$tarList") <(echo "$tarListOld"))
